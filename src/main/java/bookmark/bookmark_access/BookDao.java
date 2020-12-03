ee7b62b1d2e46c11179308ab47168cbc505b1b4a
larikkai <kai.larikka@kolumbus.fi>
2020-12-03 22:40:57 +0200

    Update bookDao and add necessary database functions related to tags

--- src/main/java/bookmark/bookmark_access/BookDao.java
+++ src/main/java/bookmark/bookmark_access/BookDao.java
@@ -1,13 +1,16 @@
 package bookmark.bookmark_access;
 
 import bookmark.domain.Book;
+import bookmark.domain.Tag;
 import java.util.List;
 
 public interface BookDao {
     
     List<Book> listAll();
-    void add(Book book);
+    int addBook(Book book);
     void modifyCurrentPage(int id, int page);
     void deleteBook(int id);
     Book getBookById(int id);
+    void addTag(Tag tag, int id);
+    Tag getTagByName(String name);
 }
--- src/main/java/bookmark/bookmark_access/DBDao.java
+++ src/main/java/bookmark/bookmark_access/DBDao.java
@@ -1,6 +1,7 @@
 package bookmark.bookmark_access;
 
 import bookmark.domain.Book;
+import bookmark.domain.Tag;
 import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -36,8 +37,9 @@ public class DBDao implements BookDao {
         try {
             ResultSet rs = getBooksResultSet(connection);
             while (rs.next()) {
+                ArrayList<Tag> tags = getTagsByBookId(connection, rs.getInt("id"));
                 Book book = new Book(rs.getInt("id"), rs.getString("title"), rs.getString("author"), rs.getInt("pages"),
-                        rs.getInt("currentpage"));
+                        rs.getInt("currentpage"), tags);
                 books.add(book);
             }
         } catch (SQLException e) {
@@ -52,9 +54,18 @@ public class DBDao implements BookDao {
      * @param book - Book object to be added
      */
     @Override
-    public void add(Book book) {
+    public int addBook(Book book) {
         Connection connection = connect();
-        addBookStatement(connection, book);
+        int bookId = addBookStatement(connection, book);
+        closeConnection(connection);
+        return bookId;
+    }
+    
+    @Override
+    public void addTag(Tag tag, int bookId) {
+        Connection connection = connect();
+        int tagId = addTagStatement(connection, tag);
+        insertIntoBookTagMappingTable(bookId, tagId);
         closeConnection(connection);
     }
 
@@ -122,8 +133,9 @@ public class DBDao implements BookDao {
             try {
                 connection = connect();
                 Statement statement = connection.createStatement();
-                statement.execute("CREATE TABLE Book (id INTEGER PRIMARY KEY, "
-                        + "title TEXT NOT NULL, author TEXT NOT NULL, pages INTEGER, currentpage INTEGER)");
+                createBookTable(statement);
+                createTagTable(statement);
+                createBookTagMappingTaple(statement);
             } catch (SQLException e) {
                 System.err.println(e.getMessage());
             } finally {
@@ -142,23 +154,127 @@ public class DBDao implements BookDao {
         }
         return connection;
     }
+    
+    private void createBookTable(Statement statement) throws SQLException {
+        statement.execute("CREATE TABLE Book ("
+            + "id INTEGER PRIMARY KEY, "
+            + "title TEXT NOT NULL, "
+            + "author TEXT NOT NULL, "
+            + "pages INTEGER, "
+            + "currentpage INTEGER)");
+    }
+    
+    private void createTagTable(Statement statement) throws SQLException {
+        statement.execute("CREATE TABLE Tag ("
+            + "id INTEGER PRIMARY KEY, "
+            + "name TEXT NOT NULL, "
+            + "UNIQUE(name))");
+    }
+    
+    private void createBookTagMappingTaple(Statement statement) throws SQLException {
+        statement.execute("CREATE TABLE Book_tag_mapping ("
+            + "book_id INTEGER, "
+            + "tag_id INTEGER)");
+    }
 
-    private void addBookStatement(Connection connection, Book book) {
+    private int addBookStatement(Connection connection, Book book) {
         final int eka = 1;
         final int toka = 2;
         final int kolmas = 3;
         final int neljas = 4;
         try {
+            String query = "INSERT INTO Book (title,author,pages,currentpage) VALUES (?,?,?,?)";
             PreparedStatement p = connection
-                    .prepareStatement("INSERT INTO Book (title,author,pages, currentpage) VALUES (?,?,?,?)");
+                .prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
             p.setString(eka, book.getTitle());
             p.setString(toka, book.getAuthor());
             p.setInt(kolmas, book.getNumberOfPages());
             p.setInt(neljas, book.getCurrentPage());
             p.executeUpdate();
+            ResultSet rs = p.getGeneratedKeys();
+            if (rs.next()) {
+                return rs.getInt(1);
+            }
         } catch (SQLException e) {
             System.err.println(e.getMessage());
         }
+        return 0;
+    }
+    
+    private int addTagStatement(Connection connection, Tag tag) {
+        final int eka = 1;
+        try {
+            String query = "INSERT INTO Tag (name) VALUES (?)";
+            PreparedStatement p = connection
+                .prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+            p.setString(eka, tag.getName());
+            p.executeUpdate();
+            ResultSet rs = p.getGeneratedKeys();
+            if (rs.next()) {
+                return rs.getInt(1);
+            }
+            int tagId = getTagId(connection, tag);
+            if (tagId != 0) {
+                return tagId;
+            }
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+        return 0;
+    }
+    
+    private int getTagId(Connection connection, Tag tag) {
+         final int eka = 1;
+        try {
+            String query = "select * Tag WHERE name = (?)";
+            PreparedStatement p = connection
+                .prepareStatement(query);
+            p.setString(eka, tag.getName());
+            ResultSet rs = p.executeQuery();
+            if (rs.next()) {
+                return rs.getInt("id");
+            }
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+        return 0;
+    }
+    
+    private ArrayList<Tag> getTagsByBookId(Connection connection, int id) {
+        final int eka = 1;
+        ArrayList<Tag> tags = new ArrayList<>();
+        try {
+            String query = "SELECT tag.name FROM Tag JOIN Book_tag_mapping ON tag_id = (?)";
+            PreparedStatement p = connection
+                .prepareStatement(query);
+            p.setInt(eka, id);
+            ResultSet rs = p.executeQuery();
+            while (rs.next()) {
+                tags.add(new Tag(rs.getString("name")));
+            }
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+        return tags;
+    }
+    
+    @Override
+    public Tag getTagByName(String tagName) {
+        final int eka = 1;
+        Tag tag = null;
+        ResultSet rs;
+        Connection connection = connect();
+        try {
+            PreparedStatement p = connection
+                .prepareStatement("select * FROM tag WHERE name = (?)");
+            p.setString(eka, tagName);
+            rs = p.executeQuery();
+            tag = new Tag(rs.getString("name"));
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+        closeConnection(connection);
+        return tag;
     }
 
     private ResultSet getBooksResultSet(Connection connection) {
@@ -169,6 +285,22 @@ public class DBDao implements BookDao {
             System.err.println(e.getMessage());
         }
         return null;
+    }
+    
+    private void insertIntoBookTagMappingTable(int bookId, int tagId) {
+        final int eka = 1;
+        final int toka = 2;
+        Connection connection = connect();
+        try {
+            String query = "INSERT INTO Book_tag_mapping (book_id, tag_id) VALUES (?, ?)";
+            PreparedStatement p = connection
+                .prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+            p.setInt(eka, bookId);
+            p.setInt(toka, tagId);
+            p.executeUpdate();
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
     }
 
     private void closeConnection(Connection connection) {
