93f6b4dce00d87dad9cd170477a60b7d1dd0cf75
larikkai <kai.larikka@kolumbus.fi>
2020-12-03 21:50:58 +0200

    Add tag class with name or name and id

--- src/main/java/bookmark/domain/Tag.java
+++ src/main/java/bookmark/domain/Tag.java
@@ -1,12 +1,46 @@
 
 package bookmark.domain;
 
+/**
+ * Representing book.
+ */
 public class Tag {
     
     private final String name;
+    private int id;
     
+    /**
+    * Create tag with name
+    *
+    * @param tagName
+    */
     public Tag(String tagName) {
         this.name = tagName;
+    }
+    
+    /**
+    * Create tag with id and name
+    *
+    * @param tagId
+    * @param tagName
+    */
+    public Tag(int tagId, String tagName) {
+        this.name = tagName;
+        this.id = tagId;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
     }
     
 }
